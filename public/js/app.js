51f3d8f44c94027a6616697fe60485d9fcc4c6ef
Adam Bigstar <abhijeet@techmorphosis.com>
2016-08-17 11:27:20 +0530

    public assets

---
+++ public/js/app.js
@@ -0,0 +1,187 @@
+var appControllers = angular.module('app-controllers', []);
+var appDirectives = angular.module('app-directives', []);
+
+var app = angular.module('app', ['ui.router', 'ngRoute', 'ngAnimate', 'ng-jwplayer', 'app-controllers']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise("film");
+
+  $stateProvider
+    .state('home', {
+      url: "/home",
+      templateUrl: "/home"
+    })
+    .state('film', {
+      url: "/film",
+      templateUrl: "film",
+    });
+});
+
+app.config(function($interpolateProvider) {
+  $interpolateProvider.startSymbol('<%');
+  $interpolateProvider.endSymbol('%>');
+});
+
+appControllers.controller('AppController', ['$rootScope', '$scope', '$log', '$state',
+  function ($rootScope, $scope, $log, $state) {
+    console.log('app controller');
+    $state.go('film');
+
+    $rootScope.showFilm = function() {
+      $state.go('film');
+    };
+
+    $rootScope.showHome = function() {
+      $state.go('home');
+    };
+
+    $rootScope.showContact = function() {
+      $state.go('contact');
+    };
+
+  }]);
+
+appControllers.controller('FilmController', ['$scope', '$log', '$sce', 'jwplayerService',
+  function ($scope, $log, $sce, jwplayerService) {
+    $scope.file = $sce.trustAsResourceUrl('http://content.jwplatform.com/videos/hWF9vG66-TNpruJId.mp4');
+    //$scope.file = $sce.trustAsResourceUrl('https://youtu.be/zSWdZVtXT7E');
+    $scope.deviceType = 'desktop';
+    $scope.device = 'desktop';
+    $scope.mediaId = 'VID0011223344';
+    $scope.width = 480;
+    $scope.height = 270;
+    $scope.mediaTitle = 'jwplayer test video';
+    $scope.mediaDescription = 'description';
+    //var streamPageUrl = '//www.youtube.com/watch?v=8CjdLYBDUqw'
+    $scope.streamPageUrl = 'http://content.jwplatform.com/videos/hWF9vG66-TNpruJId.mp4';
+    $scope.imageUrl = '//content.jwplatform.com/thumbs/hWF9vG66-480.jpg';
+    $scope.mediaPageUrl = 'https://content.jwplatform.com/videos';
+    $scope.ott = 'bigstar';
+    $scope.genres = 'animation';
+    $scope.partners = 'acme';
+    $scope.client = "googima";
+
+    $scope.tag = 'http://pubads.g.doubleclick.net/gampad/ads?' +
+      'sz=8x8&' +
+      'iu=/316192035/preroll&' +
+      'cust_params=' +
+      'devicetype%3D' + $scope.deviceType + '%26' +
+      'width%3D' + $scope.width + '%26' +
+      'height%3D' + $scope.height + '%26' +
+      'media_id%3D' + $scope.mediaId + '%26' +
+      'media_title%3D' + $scope.mediaTitle + '%26' +
+      'stream_page_url%3D' + $scope.streamPageUrl + '%26' +
+      'page_url%3D' + $scope.mediaPageUrl + '%26' +
+      '&impl=s&' +
+      'gdfp_req=1&' +
+      'env=vp&' +
+      'output=vast&' +
+      'unviewed_position_start=1' +
+      '&url=' + $scope.streamPageUrl + '&' +
+      'description_url=' + $scope.mediaPageUrl + '&' +
+      'correlator=bcf262375404d925e970013a4f390b87';
+
+    // 'partners%3D' + $scope.partners + '%26' +
+    // 'genres%3' + $scope.genres + '%26' +
+    // 'device%3D' + $scope.device + '%26' +
+    // 'ott%3D' + $scope.ott +
+
+    $scope.schedule = {
+            'adbreak-preroll1': {
+              tag: $scope.tag,
+              offset: 'pre',
+            },
+            'adbreak-preroll2': {
+              tag: $scope.tag,
+              offset: 'pre'
+            },
+            'adbreak-preroll3': {
+              tag: $scope.tag,
+              offset: 'pre'
+            },
+            'adbreak-midroll1': {
+              tag: $scope.tag,
+              offset: 15
+            },
+            'adbreak-midroll2': {
+              tag: $scope.tag,
+              offset: 30
+            },
+            'adbreak-midroll3': {
+              tag: $scope.tag,
+              offset: 45
+            },
+            'adbreak-midroll4': {
+              tag: $scope.tag,
+              offset: 60
+            },
+          };
+
+     $scope.options = {
+        autostart: true,
+        width: "640",
+        height: "360",
+        image: $scope.imageUrl,
+        title: $scope.mediaTitle,
+        description: $scope.mediaDescription,
+        mediaid: $scope.mediaId,
+        advertising: {
+          client: $scope.client,
+          admessage: 'The ad will end in xx seconds',
+          skipoffset: "0",
+          cuetext: 'Advertisement',
+          skipmessage: 'Skip ad in xx',
+          skiptext: 'Skip',
+          vpaidmode: 'insecure',
+          companiondiv: {
+            id: 'companion-ad'
+          },
+          schedule: $scope.schedule
+          }
+        };
+
+    // worked
+    // $scope.options = {
+    //   autostart: true,
+    //   width: "640",
+    //   height: "360",
+    //   image: $scope.imageUrl,
+    //   title: $scope.mediaTitle,
+    //   description: $scope.mediaDescription,
+    //   mediaid: $scope.mediaId,
+    //   advertising: {
+    //     client: $scope.client,
+    //     schedule: {"myAds":{"offset":"pre","tag":$scope.tag}}
+    //   }
+    // };
+
+    // $scope.options = {
+    //   height: 270,
+    //   width: 480,
+    //   file: $scope.streamPageUrl,
+    //   image: $scope.imageUrl,
+    //   advertising: {
+    //     client: 'googleima',
+    //     schedule: * ,
+    //     admessage: 'The ad will end in xx seconds',
+    //     skipoffset: 15,
+    //     cuetext: 'Advertisement',
+    //     skipmessage: 'Skip ad in xx',
+    //     skiptext: 'Skip',
+    //     vpaidmode: 'enabled',
+    //     companiondiv: {
+    //       id: 'companion-ad'
+    //     }
+    //   }
+    // };
+
+    $scope.$on('ng-jwplayer-ready', function(event, args) {
+      $log.info('Player ' + args.playerId + ' ready. Playing video');
+      var player = jwplayerService.myPlayer[args.playerId];
+      //player.play(true);
+    });
+
+  }]);
+
+//# sourceMappingURL=app.js.map
